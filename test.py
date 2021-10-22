4d90a032f3b51694a41f2bc06ce4bae23e6b80b9
Leowrin <leo.dana@epfl.ch>
2021-10-22 23:16:37 +0200

    updated .py

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO #uncomment on rPi
+#import serial
+import time
+
+ser = serial.Serial("/dev/Serial0") ###https://pyserial.readthedocs.io/en/latest/shortintro.html#opening-serial-ports
+ser.baudrate = 9600
+while 1:
+    try:
+        data = ser.read()
+    except KeyboardInterrupt:
+        break
