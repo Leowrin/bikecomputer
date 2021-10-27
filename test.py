930a7dcb3668038ff581365894d4b3d6a99f41d5
leowrin <leo.dana.30@gmail.com>
2021-10-27 18:31:39 +0200

    exception added

--- test.py
+++ test.py
@@ -4,16 +4,24 @@ import time
 
 ser = serial.Serial("/dev/serial0", baudrate=9600, timeout=1) ###https://pyserial.readthedocs.io/en/latest/shortintro.html#opening-serial-ports
 
-gpga = ""
+gpgga = ""
+#coordinates[][]
 
 while 1:
     try:
-        text = ser.readline()
+        try:
+            text = ser.readline()
+        except:
+            print("failed to read GPS")
         text = str(text)
+        print(text)
         data = text.split(",")
         if data[0] == "$GPGGA":
+            gpgga.append(data[2])
             gpgga.append(data[4])
-            gpgga.append(data[6])
+            gpgga.append(data[9])
+            coordinates.append(gpgga) #à retravailler
 
     except KeyboardInterrupt:
         break
+
