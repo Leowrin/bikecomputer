0cf9e8150fdc36efa1b3ecde2a814726c64aa5f0
Leowrin <leo.dana@epfl.ch>
2021-10-27 13:33:03 +0200

    .

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-#import RPi.GPIO as GPIO #uncomment on rPi
-#import serial
+import RPi.GPIO as GPIO #uncomment on rPi
+import serial
 import time
 
 ser = serial.Serial("/dev/Serial0", baudrate=9600, timeout=1) ###https://pyserial.readthedocs.io/en/latest/shortintro.html#opening-serial-ports
@@ -8,6 +8,7 @@ while 1:
     try:
         text = ser.readline()
         data = text.split(',')
+        print(text)
         if data[0] == "$GPGGA":
             ### continue
 
