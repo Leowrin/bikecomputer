d6c5d96f080aa2e33dae1642b64603c36d2236c7
leowrin <leo.dana.30@gmail.com>
2021-10-27 14:23:04 +0200

    sudo resolved

--- test.py
+++ test.py
@@ -4,15 +4,16 @@ import time
 
 ser = serial.Serial("/dev/serial0", baudrate=9600, timeout=1) ###https://pyserial.readthedocs.io/en/latest/shortintro.html#opening-serial-ports
 
+gpga = ""
+
 while 1:
     try:
         text = ser.readline()
         text = str(text)
-        print(type(text))
         data = text.split(",")
-        print(text)
-#         if data[0] == "$GPGGA":
-#             pass
+        if data[0] == "$GPGGA":
+            gpgga.append(data[4])
+            gpgga.append(data[6])
 
     except KeyboardInterrupt:
         break
