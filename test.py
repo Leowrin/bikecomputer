c6ec0f5bf5a117e87ff2ddbb61579cb8216bda22
leowrin <leo.dana.30@gmail.com>
2021-10-27 13:51:00 +0200

    .

--- test.py
+++ test.py
@@ -2,15 +2,17 @@ import RPi.GPIO as GPIO #uncomment on rPi
 import serial
 import time
 
-ser = serial.Serial("/dev/Serial0", baudrate=9600, timeout=1) ###https://pyserial.readthedocs.io/en/latest/shortintro.html#opening-serial-ports
+ser = serial.Serial("/dev/serial0", baudrate=9600, timeout=1) ###https://pyserial.readthedocs.io/en/latest/shortintro.html#opening-serial-ports
 
 while 1:
     try:
         text = ser.readline()
-        data = text.split(',')
+        text = str(text)
+        print(type(text))
+        data = text.split(",")
         print(text)
-        if data[0] == "$GPGGA":
-            pass
+#         if data[0] == "$GPGGA":
+#             pass
 
     except KeyboardInterrupt:
         break
