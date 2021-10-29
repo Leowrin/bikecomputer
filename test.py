3d96869649bba50899b82f848980e64b3ce1dfec
leowrin <leo.dana.30@gmail.com>
2021-10-29 11:24:19 +0200

    bugfix

--- test.py
+++ test.py
@@ -10,6 +10,10 @@ coordinates = np.empty((0, 3))
 filename = str(time.strftime("%Y_%m_%d_%H_%M", time.localtime())) + ".csv"
 
 
+#def parse_gps(data):
+    
+
+
 while 1:
     try:
         try:
@@ -19,8 +23,14 @@ while 1:
 
         text = str(text)
         data = text.split(",")
-
-        if data[0] == "$GPGGA" or data[0] == "b'$GPGGA":
+        print(text)
+        try :
+            tmp = float(data[5])
+            
+        except :
+            tmp = 0
+            
+        if (data[0] == "$GPGGA" or data[0] == "b'$GPGGA") and tmp>0:
             print("YES")
             lat = float(data[2])
             lon = float(data[4])
@@ -29,7 +39,8 @@ while 1:
             print(np.shape(coordinates))
             coordinates = np.append(coordinates, [lat, lon, alt], axis=0) #append cree une copie, le = est necessaire. a mettre directement et reshape !
             print(coordinates)
-
+        #else :
+        #    print("no GPS signal or no GPGGA serial input")
 
     except KeyboardInterrupt:
         np.savetxt(filename)
