e491a3529f55fa82f2e86ec539b2a4106a85dc5f
leowrin <leo.dana.30@gmail.com>
2021-10-29 13:56:20 +0200

    csv file saved successfully !

--- test.py
+++ test.py
@@ -5,16 +5,17 @@ import numpy as np
 
 ser = serial.Serial("/dev/serial0", baudrate=9600, timeout=1) ###https://pyserial.readthedocs.io/en/latest/shortintro.html#opening-serial-ports
 
-coordinates = np.array(())
+coordinates = np.empty((0, 3), dtype=float)
 
 filename = str(time.strftime("%Y_%m_%d_%H_%M", time.localtime())) + ".csv"
 
 
 #def parse_gps(data):
     
+count=0
 
-
-while 1:
+while count<10 :
+    
     try:
         try:
             text = ser.readline()
@@ -24,29 +25,31 @@ while 1:
         text = str(text)
         if text.split(",")[0] == "$GPGGA" or text.split(",")[0] == "b'$GPGGA" :
             data = text.split(",")
-            print(data)
             
             try :
                 tmp = float(data[7])
+                tmp1 = float(data[2])
             
             except :
                 tmp = 0
             
             if tmp>0 :
-                print(data)
                 lat = float(data[2])
                 lon = float(data[4])
                 alt = float(data[9])
 
-                print(np.shape(coordinates))
-                print(np.shape([[lat, lon, alt]]))
-                coordinates = np.concatenate((coordinates, [[lat], [lon], [alt]]), axis=0)
+                
+                coordinates = np.concatenate((coordinates, [[lat, lon, alt]]), axis=0)
                 print(coordinates)
+                count+=1
             
         #else :
         #    print("no GPS signal or no GPGGA serial input")
 
     except KeyboardInterrupt:
-        np.savetxt(filename)
+        
         #terminer python et lancer C
         break
+    
+np.savetxt(filename, coordinates, delimiter=',')
+exit()
