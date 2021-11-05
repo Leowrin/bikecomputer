a4cc272086f7ccdd0bd229b1d53f279aed43a866
Leowrin <leo.dana@epfl.ch>
2021-11-05 14:43:06 +0100

    deltaH + problem with e notation

--- test.py
+++ test.py
@@ -11,11 +11,11 @@ filename = str(time.strftime("%Y_%m_%d_%H_%M", time.localtime())) + ".csv"
 
 
 #def parse_gps(data):
-    
+
 count=0
 
 while count<10 :
-    
+
     try:
         try:
             text = ser.readline()
@@ -25,35 +25,36 @@ while count<10 :
         text = str(text)
         if text.split(",")[0] == "$GPGGA" or text.split(",")[0] == "b'$GPGGA" :
             data = text.split(",")
-            
+
             try :
                 ### check nb satellites et coo
                 satellites = int(data[7])
                 float(data[2])
-            
+
             except :
                 satellites = 0
-            
+
             ### précision vs sample rate, tmp>0 pour toutes valeurs, satellites > 3 pour précision max
             if satellites>3 :
                 lat = float(data[2])
                 lon = float(data[4])
                 alt = float(data[9])
 
-                
+
                 coordinates = np.concatenate((coordinates, [[lat, lon, alt]]), axis=0)
                 count+=1
-            
+
         #else :
         #    print("no GPS signal or no GPGGA serial input")
 
     except KeyboardInterrupt:
-        
+
         #terminer python et lancer C
         break
 ### save en csv
 
-np.savetxt(filename, coordinates, delimiter=',')
+### %f ou %1.10f ?????????????????????????????????????????????????????????????????????????????????????
+np.savetxt(filename, coordinates, fmt='%f', delimiter=',')
 
 ### lancer le code C avec la longueur du fichier en argument. coordinates.shape[0]
 exit()
--- test_suite.c
+++ test_suite.c
@@ -34,3 +34,44 @@ bool readCsv(char * filename, double * values, int sizeX, int sizeY) {
     fclose(file);
     return true;
 }
+
+
+
+
+int main(int argc, char *argv[]) {
+
+  //lecture des arguments
+  /*if (argc != 2) {
+    return 1;
+  }*/
+  char * filename = argv[1];
+  int csvLen = 1000; //atoi(argv[2]);
+
+
+  //creation du bloc memoire pour les points gps
+  double * gpsPoints = malloc(1000 * 3 * sizeof (double));
+  //lecture du fichier et copie dans memoire ^.
+  readCsv(filename, gpsPoints, 1000, 3);
+
+  //creation d'une variable deltaH
+  double * deltaH = malloc(csvLen-1 * 1 * sizeof (double));
+
+  
+  for (size_t i = 0; i < 100; i++) {
+    int j = i+1;
+    deltaH[i] = gpsPoints[j * 3 + 2] - gpsPoints[i * 3 + 2];
+  }
+
+  for (size_t i = 0; i < 3000; i++) {
+    printf("%f\n", gpsPoints[i]);
+  }
+
+
+
+
+
+
+
+
+  return 0;
+}
