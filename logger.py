27a1ac23df9d8913e30f2825f1132790e1b3fefd
leowrin <leo.dana.30@gmail.com>
2021-12-01 18:13:18 +0100

    closed issue serread buffer

--- logger.py
+++ logger.py
@@ -7,27 +7,37 @@ import math
 from smbus import SMBus ### RPI only
 from bmp280 import BMP280 ### RPI only
 
-ser = serial.Serial("/dev/serial0", baudrate=9600, timeout=1) ###https://pyserial.readthedocs.io/en/latest/shortintro.html#opening-serial-ports
+ ###https://pyserial.readthedocs.io/en/latest/shortintro.html#opening-serial-ports
 bmp280 = BMP280(i2c_dev=SMBus(1))
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(36, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-coordinates = np.empty((0, 4), dtype=float)
+button = 36
 
-folder = "logs/"
-folder += str(time.strftime("%Y_%m_%d_%H_%M", time.localtime())) + '/'
-filename = "logged_data.csv"
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-try :
-    cmd = "mkdir " + folder
-    os.system(cmd)
 
-except :
-    cmd = "mkdir -p " + folder
-    os.system(cmd)
+def terminate(folder, filename, coordinates):
+    try :
+        np.savetxt(folder+filename, coordinates, fmt='%f', delimiter=',')
+        print("DONE")
 
-#def parse_gps(data):
+        ### lancer le code C avec la longueur du fichier en argument. coordinates.shape[0]
+        cmd = "./gps " + folder+filename + ' ' + str(coordinates.shape[0]) + ' ' + str(coordinates.shape[1]) + " > " + folder + "computed_data.csv"
+        os.system(cmd)
+
+        ### debug
+        time.sleep(1)
+
+        os.system("python3 plotter.py " + folder)
+        
+        ser.close()
+
+        return 1
+
+    except :
+        return 0
+
 
 count = 0
 lat = 0.0
@@ -37,27 +47,63 @@ alt = 0.0
 ### debug
 bmp280.get_pressure()
 
-while count<10 :
 
-    try:
-        try:
-            text = ser.readline()
-        except:
-            print("failed to read GPS")
+stateA = 0
+stateB = 0
 
-        text = str(text)
-        if text.split(",")[0] == "$GPGGA" or text.split(",")[0] == "b'$GPGGA" :
-            data = text.split(",")
+
+while 1 :
+    while GPIO.input(button) == 0 :
+        stateB = 1
+        text = None
+        
+        if stateA == 0 :
+            coordinates = np.empty((0, 4), dtype=float)
+
+            folder = "logs/"
+            folder += str(time.strftime("%Y_%m_%d_%H_%M", time.localtime())) + '/'
+            filename = "logged_data.csv"
 
             try :
-                ### check nb satellites et coo
-                satellites = int(data[7])
-                float(data[2])
+                cmd = "mkdir " + folder
+                os.system(cmd)
 
             except :
-                satellites = 0
+                try :
+                    cmd = "mkdir -p " + folder
+                    os.system(cmd)
+                except :
+                    pass
+            stateA = 1
+        
+        try:
+            ser = serial.Serial("/dev/serial0", baudrate=9600, timeout=1.1)
+        except:
+            print("erreur init com serial0")
+            
+        try:
+            text = ser.readline()
+            text = str(text)
+                
+        except:
+            print("failed to read GPS")
+            text = None
 
-            ### précision vs sample rate, tmp>0 pour toutes valeurs, satellites > 3 pour précision
+
+        try :
+            ### check nb satellites et coo
+            satellites = int(data[7])
+            float(data[2])
+
+        except :
+            satellites = 0
+
+
+
+        if text.split(",")[0] == "$GPGGA" or text.split(",")[0] == "b'$GPGGA" :
+            print(text)
+            data = text.split(",")
+        ### précision vs sample rate, tmp>0 pour toutes valeurs, satellites > 3 pour précision
             if satellites>3 :
                 lat = float(data[2])/100
                 lon = float(data[4])/100
@@ -76,36 +122,15 @@ while count<10 :
                 lon += tmp*100
                 lon = round(lon, 7)
 
-            """else :
-                ##### A ENVOYER si pas de signal GPS
-                lat = None
-                lon = 0"""
-
             pressure = bmp280.get_pressure() ### envoie donnees pression meme sans GPS
 
             coordinates = np.concatenate((coordinates, [[lat, lon, alt, pressure]]), axis=0)
+            print(count)
             count+=1
 
-        #else :
-        #    print("no GPS signal or no GPGGA serial input")
+    if stateB == 1 :
+        terminate(folder, filename, coordinates)
+        stateB = 0
+        
+    stateA = 0
 
-    except KeyboardInterrupt:
-
-        #terminer python et lancer C
-        break
-    
-### save en csv
-np.savetxt(folder+filename, coordinates, fmt='%f', delimiter=',')
-print("DONE")
-
-### lancer le code C avec la longueur du fichier en argument. coordinates.shape[0]
-cmd = "./gps " + folder+filename + ' ' + str(coordinates.shape[0]) + ' ' + str(coordinates.shape[1]) + " > " + folder + "computed_data.csv"
-os.system(cmd)
-
-### debug
-time.sleep(1)
-
-os.system("python3 plotter.py " + folder)
-
-
-exit()
