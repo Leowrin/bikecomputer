f5e4df25f8d7ae6974dd4648468e23c7ea79e0ee
leowrin <leo.dana.30@gmail.com>
2021-12-12 14:47:41 +0100

    bike test

--- compute.c
+++ compute.c
@@ -261,6 +261,7 @@ int main(int argc, char *argv[]) {
     if (deltaHPres[i] > 0) {
       // /3600 pour wattheure au lieu de joules
       workgPres[i] += (mass * 9.81 * deltaHPres[i]) / 3600;
+      workgPres[i] -= 0.004 / 90 * mass;
     }
   }
 
