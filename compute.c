31948ed8cd0f15e138f44e7e5589ebe3d8bd8668
Leowrin <leo.dana@epfl.ch>
2021-12-15 18:40:03 +0100

    bugfix

--- compute.c
+++ compute.c
@@ -166,7 +166,7 @@ int main(int argc, char *argv[]) {
           lv95Y[j] = lv95Y[i];
         }
         break;
-      if (i == csvLen) {
+      if (i >= csvLen) {
         break;
       }
       }
