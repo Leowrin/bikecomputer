491dfc78e2c6aad0181a9374fd9d21e1c8c790a3
Leowrin <leo.dana@epfl.ch>
2021-12-19 14:26:09 +0100

    update

--- logger.py
+++ logger.py
@@ -1,3 +1,5 @@
+#! /usr/bin/python3
+
 import RPi.GPIO as GPIO #uncomment on rPi
 import serial
 import time
@@ -8,8 +10,10 @@ from smbus import SMBus ### RPI only
 from bmp280 import BMP280 ### RPI only
 import config
 
+### ouvre la communication avec le BMP280
 bmp280 = BMP280(i2c_dev=SMBus(config.i2c_bus))
 
+### utilise la numerotation BOARD pour les pins
 GPIO.setmode(GPIO.BOARD)
 
 
@@ -22,19 +26,17 @@ ser = serial.Serial("/dev/serial0", baudrate=9600, timeout=1)
 ser.close()
 
 def terminate(folder, filename, coordinates):
-    
-    print(folder+filename)
+
     np.savetxt(folder+filename, coordinates, fmt='%f', delimiter=',')
 
-      ### lancer le code C avec la longueur du fichier en argument. coordinates.shape[0]
-    
+    ### lancer le code C avec les arguments necessaires
     cmd = "/home/pi/bikecomputer/gps "
     cmd += folder+filename + ' '
     cmd += str(coordinates.shape[0]) + ' ' + str(coordinates.shape[1]) + ' ' + str(config.mass)
     cmd += " > " + folder + "computed_data.csv"
     os.system(cmd)
 
-        ### debug
+    ### debug
     time.sleep(1)
 
     os.system("python3 /home/pi/bikecomputer/plotter.py " + folder)
@@ -43,11 +45,10 @@ def terminate(folder, filename, coordinates):
 
     return 1
 
-    
-    
-    
 
-count = 0
+
+
+### initialisation des variables
 lat = 0.0
 lon = 0.0
 alt = 0.0
@@ -59,12 +60,15 @@ bmp280.get_pressure()
 stateA = 0
 stateB = 0
 
-
+### boucle infinie
 while 1 :
+
+    ### enregistre les donnees tant que le recordSwitch_gpio est connecte au GND
     while GPIO.input(config.recordSwitch_gpio) == 0 :
         stateB = 1
         text = None
 
+        ### avant le premier enregistrement, reinitialise certaines variables, allume la LED, cree le dossier avec la date
         if stateA == 0 :
             ### debug
             bmp280.get_pressure()
@@ -116,10 +120,10 @@ while 1 :
             satellites = 0
 
 
-
+        ### verifie si une trame GPGGA est recue
         if text.split(",")[0] == "$GPGGA" or text.split(",")[0] == "b'$GPGGA" :
             data = text.split(",")
-            ### précision vs sample rate, tmp>0 pour toutes valeurs, satellites > 3 pour précision
+            ### précision vs sample rate, satellites > 3 pour précision
             if satellites>3 :
                 try:
                     lat = float(data[2])/100
@@ -130,7 +134,7 @@ while 1 :
                     lon = 0
                     alt = 0
 
-                ### convertion from minutes' to decimal
+                ### conversion degres minutes -> degres decimaux
                 tmp = lat - math.floor(lat)
                 tmp /= 60
                 lat = math.floor(lat)
@@ -146,13 +150,13 @@ while 1 :
             pressure = bmp280.get_pressure() ### envoie donnees pression meme sans GPS
 
             coordinates = np.concatenate((coordinates, [[lat, lon, alt, pressure]]), axis=0)
-            print(count)
-            count+=1
 
+
+
+    ### si le recordSwitch_gpio est decoconnecte du GND, lance la fonction terminate et eteint la LED
     if stateB == 1 :
         terminate(folder, filename, coordinates)
         stateB = 0
-        count = 0
         GPIO.output(config.recordLED_gpio, GPIO.LOW)
 
     stateA = 0
